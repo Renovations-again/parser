20edc45d65b673995848dc2b72cc6e9cd7975cc7
David-Roklem <roklem@mail.ru>
2023-09-14 11:20:59 +0300

    petrovich prices
    Теперь цены в petrovich отображаются по всем единицам
    измерения. Также преобразовал цены из int в float в
    oboykin и vodopad

--- shops/petrovich.py
+++ shops/petrovich.py
@@ -1,34 +1,133 @@
 import requests
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 URL = 'https://moscow.petrovich.ru/catalog/9575968/987260/'  # стул
 URL2 = 'https://moscow.petrovich.ru/catalog/12454/685400/'  # дверь
 URL3 = 'https://moscow.petrovich.ru/catalog/245046398/620133/'  # плитка
 
-request = requests.get(URL3)
+CURRENT_URL = URL3
+
+request = requests.get(CURRENT_URL)
 soup = BeautifulSoup(request.text, 'lxml')
 
-h1 = soup.find('h1')
-code = soup.find('span', class_='pt-c-secondary-lowest').next_sibling
-images = soup.find_all('img', class_="swiper-lazy")
-link_images = []
 
-prices = soup.find_all('div', class_='price-details')
-
-units = soup.find('span', class_='units-tabs').find_all('span', class_='unit-tab')
-
-for image in images:
-    src = image.get('src')
-    link_images.append(src)
-
-print(h1.text, code.text, set(link_images), sep='\n')
-
-for unit in units:
-    print('Цена за', unit.get_text())
+def get_title():
+    '''Get the name of an item (Название товара)'''
+    title = soup.find('h1').text
+    return title
 
 
-gold_price = soup.find('p', class_='gold-price')
-retail_price = soup.find('p', class_='retail-price')
+def get_vendor_code():
+    '''Get vendor code of an item (Артикул товара)'''
+    vendor_code = soup.find(
+        'span', class_='pt-c-secondary-lowest'
+    ).next_sibling.text
+    return vendor_code
 
-print('Цена по карте:', gold_price.text)
-print('Цена:', retail_price.text)
+
+def get_images():
+    '''Returns a list of all the images of an item (Ссылки на картинки)'''
+    images = soup.find_all('img', class_="swiper-lazy")
+    link_images = []
+    for image in images:
+        src = image.get('src')
+        link_images.append(src)
+    return list(set(link_images))
+
+
+def get_price():
+    '''Get the price of an item (Цена товара)'''
+    prices = soup.find('div', class_='price-details')
+    units = soup.find('span', class_='units-tabs')
+
+    # Если детей (тегов <p>) > 1, то цены две: по карте и обычная
+    children_price = list(prices.children)
+    children_unit = list(units.children)
+
+    # Прасим цены для разных единиц измерения (units)
+    with sync_playwright() as p:  # noqa: E999
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(CURRENT_URL)
+        # Если len(children_unit) > 2, то юнитов цены больше одного
+        if len(children_unit) > 2:
+            unit_text = page.query_selector('span[class="active unit-tab"]')
+            print('Цена за ', unit_text.text_content())
+            if len(children_price) == 1:
+                print('Цена:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+            else:
+                print('Цена по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+                print('Цена не по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-retail-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+            unit_text = page.query_selector('span[class="unit-tab"]')
+            unit_text.click()
+
+            print('/'*50)
+            print('Цена за ', unit_text.text_content())
+
+            if len(children_price) == 1:
+                print('Цена:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+            else:
+                print('Цена по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+                print('Цена не по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-retail-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+        else:
+            print('Цена за ', children_unit[1].text)
+            if len(children_price) == 1:
+                print('Цена:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+            else:
+                print('Цена по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-gold-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+                print('Цена не по карте:', float(
+                    page.query_selector(
+                        'p[data-test="product-retail-price"]'
+                    ).text_content().replace('₽', '').replace(' ', '').
+                    replace(',', '.')
+                ))
+
+
+print('Название:', get_title())
+print('------------')
+print('Артикул:', get_vendor_code())
+print('------------')
+print(get_price())
+print('------------')
+print('Список ссылок на картинки:', get_images())
