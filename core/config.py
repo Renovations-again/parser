825760c7db10b5e015e0e76d7d34de7698a96075
David-Roklem <roklem@mail.ru>
2023-09-30 20:23:25 +0300

    add DB settings to core/config.py

--- core/config.py
+++ core/config.py
@@ -8,6 +8,16 @@ class Settings(BaseSettings):
                             'Артикул, Цену, Единицу измерения '
                             'и Список изображений товара.')
 
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def db_url_asyncpg(self):
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'  # noqa
+
     class Config:
         env_file = '.env'
 
