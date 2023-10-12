9c64cc4a0611ddcfbfa195eb3442a8d00e202f7f
David-Roklem <roklem@mail.ru>
2023-10-12 11:57:12 +0300

    Add models

---
+++ db/models.py
@@ -0,0 +1,127 @@
+from uuid import uuid4
+
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy import Numeric
+from sqlalchemy import TIMESTAMP
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    '''Base model derived from DeclarativeBase class.'''
+
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+        unique=True,
+    )
+
+    def __repr__(self) -> str:
+        '''Representation of a string object.
+
+        Returns:
+            object: string object
+        '''
+        return self.__class__.__name__
+
+    __str__ = __repr__
+
+
+class Product(Base):
+    '''Product sqlalchemy model.'''
+
+    __tablename__ = 'products'
+
+    title: Mapped[str] = mapped_column(String(250))
+    url: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    unit: Mapped[str] = mapped_column(String(50))
+    vendor_code: Mapped[str] = mapped_column(String(50), index=True)
+    shop_id: Mapped[int] = mapped_column(ForeignKey('shop.id'))
+    city_id: Mapped[int] = mapped_column(ForeignKey('cities.id'))
+    shop: Mapped['Shop'] = relationship(
+        back_populates='products', cascade='all, delete-orphan'
+    )
+    city: Mapped['City'] = relationship(back_populates='products')
+
+    def __repr__(self):
+        '''Representation of a string object.
+
+        Returns:
+            object: string object
+        '''
+        return '<City id: {id}, title: {title}>'.format(
+            id=self.id,
+            title=self.title,
+        )
+
+
+class Shop(Base):
+    '''Shop sqlalchemy model.'''
+
+    __tablename__ = 'shops'
+
+    title: Mapped[str] = mapped_column(String(250))
+    product: Mapped['Product'] = relationship(back_populates='shop')
+
+    def __repr__(self):
+        '''Representation of a string object.
+
+        Returns:
+            object: string object
+        '''
+        return '<Shop id: {id}, title: {title}>'.format(
+            id=self.id,
+            title=self.title,
+        )
+
+
+class City(Base):
+    '''Price sqlalchemy model.'''
+
+    __tablename__ = 'cities'
+
+    title: Mapped[str] = mapped_column(String(250), unique=True)
+    price_id: Mapped[int] = mapped_column(ForeignKey('prices.id'))
+    product: Mapped['Product'] = relationship(back_populates='cities')
+
+    def __repr__(self):
+        '''Representation of a string object.
+
+        Returns:
+            object: string object
+        '''
+        return '<Price id: {id}, title: {title}>'.format(
+            id=self.id,
+            title=self.title,
+        )
+
+
+class Price(Base):
+    '''Price sqlalchemy model.'''
+
+    __tablename__ = 'prices'
+
+    price: Mapped[float] = mapped_column(Numeric(10, 2))
+    date = mapped_column(
+        TIMESTAMP,
+        server_default=func.current_timestamp(),
+        nullable=False,
+    )
+    city: Mapped['City'] = relationship(back_populates='price', uselist=False)
+
+    def __repr__(self):
+        '''Representation of a string object.
+
+        Returns:
+            object: string object
+        '''
+        return '<Price id: {id}, title: {title}>'.format(
+            id=self.id,
+            title=self.title,
+        )
