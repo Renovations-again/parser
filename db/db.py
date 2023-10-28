03119d454c8ee139cd9ac392d41f01564de9ff92
David-Roklem <roklem@mail.ru>
2023-10-28 13:26:59 +0300

    Redistribute db settings into proper modules

--- db/db.py
+++ db/db.py
@@ -1,31 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from pydantic_settings import BaseSettings
+from parsing.core.config import Settings
 
-
-class Settings(BaseSettings):
-    app_title: str = 'Парсер строительных магазинов'
-    app_description: str = ('Имеется 5 магазинов. У каждого свой эндпоинт. '
-                            'Передаем ссылку на товар - получаем Название, '
-                            'Артикул, Цену, Единицу измерения '
-                            'и Список изображений товара.')
-
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def db_url_asyncpg(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'  # noqa
-
-    class Config:
-        env_file = '.env'
-
-
-settings = Settings()
 
 async_engine = create_async_engine(
-    settings.db_url_asyncpg, future=True, echo=True
+    Settings.db_url_asyncpg, future=True, echo=True
 )
