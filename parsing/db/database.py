10dac3a7fd184c45db2cdc38bf285fe232c06df9
David-Roklem <roklem@mail.ru>
2023-11-14 15:25:07 +0300

    Update db configuration

--- parsing/db/database.py
+++ parsing/db/database.py
@@ -1,4 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, async_sessionmaker, AsyncSession
+)
 
 from parsing.core.config import settings
 
@@ -6,3 +9,20 @@ from parsing.core.config import settings
 async_engine = create_async_engine(
     settings.db_url_asyncpg, future=True, echo=True
 )
+async_session_maker = async_sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_async_session() -> AsyncGenerator:
+    '''Get sqlalchemy async session.
+
+    yield:
+        object: sqlalchemy async session
+    '''
+    async_session = async_sessionmaker(
+        bind=async_engine,
+        expire_on_commit=False,
+    )
+    async with async_session() as session:
+        yield session
--- parsing/db/models.py
+++ parsing/db/models.py
@@ -133,11 +133,3 @@ class Price(Base):
             id=self.id,
             title=self.title,
         )
-
-
-async def init_models():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-asyncio.run(init_models())
