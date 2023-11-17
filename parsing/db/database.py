f73cedd2ac76f9f3c66e53ac5be4903468f69426
David-Roklem <roklem@mail.ru>
2023-11-17 18:39:59 +0300

    turn off AsyncGenerator in database.py

--- parsing/db/database.py
+++ parsing/db/database.py
@@ -13,16 +13,16 @@ async_session_maker = async_sessionmaker(
     async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
+# Пока не понятно зачем вообще использовать этот AsyncGenerator
+# async def get_async_session() -> AsyncGenerator:
+#     '''Get sqlalchemy async session.
 
-async def get_async_session() -> AsyncGenerator:
-    '''Get sqlalchemy async session.
-
-    yield:
-        object: sqlalchemy async session
-    '''
-    async_session = async_sessionmaker(
-        bind=async_engine,
-        expire_on_commit=False,
-    )
-    async with async_session() as session:
-        yield session
+#     yield:
+#         object: sqlalchemy async session
+#     '''
+#     async_session = async_sessionmaker(
+#         bind=async_engine,
+#         expire_on_commit=False,
+#     )
+#     async with async_session() as session:
+#         yield session
